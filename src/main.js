69bb21c7922c0fc6acaa395d80405b8c1ddd70d0
RiFrost <ri.frost@web.de>
2022-11-09 16:10:34 +0100

    add tailwindcss and amplify backend

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import AmplifyVue from '@aws-amplify/ui-vue';
+
+Amplify.configure(awsExports)
+
 import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(AmplifyVue)
 app.use(createPinia())
 app.use(router)
 
