1bc23ad2a6a4ebd9d2ee1123a6ac1a24094245ce
RiFrost <ri.frost@web.de>
2022-11-21 23:11:04 +0100

    update css; remove unused dependency

--- src/App.vue
+++ src/App.vue
@@ -42,3 +42,23 @@ async function sendAPIRequest() {
   <!-- <button @click="sendAPIRequest">Test Request</button>
   <p>Result: {{ res }}</p> -->
 </template>
+
+<style>
+.amplify-button[data-variation="link"]:hover {
+  background: none;
+  text-decoration: underline;
+  text-underline-offset: 3px;
+}
+
+.amplify-button[data-variation="primary"] {
+  background: var(--amplify-colors-teal-80);
+}
+
+.amplify-button[data-variation="primary"]:hover {
+  background-color: var(
+    --amplify-components-button-primary-hover-background-color
+  );
+  border-color: var(--amplify-components-button-primary-hover-border-color);
+  color: var(--amplify-components-button-primary-hover-color);
+}
+</style>
