ba29b1f2a5bda6105df704a8f1e67e9fc322e482
RiFrost <ri.frost@web.de>
2022-11-21 23:15:39 +0100

    remove unused code

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,5 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
-import { useAuthStore } from "@/stores/auth";
-import axios from "axios";
-import { ref } from "vue";
-
-const authStore = useAuthStore();
-
-authStore.authUser();
-const res = ref(null);
-
-async function sendAPIRequest() {
-  const headers = {
-    Authorization: authStore.getAccessToken,
-    "Content-type": "Application/json",
-  };
-  const data = {
-    text: "Lorem Ipsum",
-    desiredLang: "SP",
-  };
-
-  axios
-    .post(`${import.meta.env.VITE_POST_API_ENDPOINT}/translate`, data, {
-      headers,
-    })
-    .then(function (response) {
-      console.log(response.data.body);
-      res.value = response;
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.value = error;
-    });
-}
 </script>
 
 <template>
@@ -39,8 +7,6 @@ async function sendAPIRequest() {
     <Navbar />
   </header>
   <RouterView />
-  <!-- <button @click="sendAPIRequest">Test Request</button>
-  <p>Result: {{ res }}</p> -->
 </template>
 
 <style>
