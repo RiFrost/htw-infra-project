6ca883a74a6be3a262054bea947f7e0345541be6
RiFrost <ri.frost@web.de>
2022-11-22 09:01:50 +0100

    update css

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,4 @@ import Navbar from "@/components/Navbar.vue";
   <RouterView />
 </template>
 
-<style>
-.amplify-button[data-variation="link"]:hover {
-  background: none;
-  text-decoration: underline;
-  text-underline-offset: 3px;
-}
-
-.amplify-button[data-variation="primary"] {
-  background: var(--amplify-colors-teal-80);
-}
-
-.amplify-button[data-variation="primary"]:hover {
-  background-color: var(
-    --amplify-components-button-primary-hover-background-color
-  );
-  border-color: var(--amplify-components-button-primary-hover-border-color);
-  color: var(--amplify-components-button-primary-hover-color);
-}
-</style>
+<style></style>
